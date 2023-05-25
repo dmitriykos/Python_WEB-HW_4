ca72a3d982bb55294b8886c4f6ef5ac8419795ef
Dmytro Kosytsin <dmitriykositsyn@gmail.com>
2023-05-25 23:33:04 +0300

    third commit

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, unquote_plus
 import mimetypes
+import pathlib
 
 class MyHandler(BaseHTTPRequestHandler):
 
-    def go_GET(self):
+    def send_static(self):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        if mt:
+            self.send_header('Content-type', mt[0])
+        else:
+            self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        with open(f'.{self.path}', 'rb') as file:
+            self.wfile.write(file.read())
+
+
+    def do_GET(self):
+
         url = urlparse(self.path)
 
         if url.path == '/':
@@ -12,26 +26,34 @@ class MyHandler(BaseHTTPRequestHandler):
         elif url.path == '/message':
             self.render_template('message.html')
         else:
-            self.render_template('error.html')
-
-
+            if pathlib.Path().joinpath(url.path[1:]).exists():
+                self.send_static()
+            else:
+                self.render_template('error.html')
+             
     def do_POST(self):
         raw_data = self.rfile.read()
         data = unquote_plus(raw_data.decode())
-        data = self.parse_fro
+        data = self.parse_form_data(data)
 
-    def parse_from_data(self, data):
-        raw_data = data.split('&')
-        data = {key: value}
+        print(data)
+
+    def parse_form_data(self, data):
+        raw_params = data.split('&')
+        data = {key: value for key, value in [param.split('=') for param in raw_params]}
+        return data
 
     def render_template(self, html_page):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
 
-        with open(html_page, 'rb') as file:
-            self.wfile.write(file.read())
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            with open(html_page, 'rb') as file:
+                self.wfile.write(file.read())
+
 
 if __name__ == '__main__':
+
     server = HTTPServer(('localhost', 3000), MyHandler)
     server.serve_forever()
